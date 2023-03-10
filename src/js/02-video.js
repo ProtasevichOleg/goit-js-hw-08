2d25a0383dd54776694af8b0a89f15bfa86d004a
Oleh Protasevych <protasevicholeg@gmail.com>
2023-03-10 03:03:58 +0200

    Try to fix "savePlaybackPosition" function again
    merging it back into the main code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-import savePlaybackPosition from './utils/savePlaybackPosition';
+// import savePlaybackPosition from './utils/savePlaybackPosition';
 
 const player = new Player('vimeo-player');
 const currentTime = localStorage.getItem('videoplayer-current-time');
+
+function savePlaybackPosition(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
 
 if (!currentTime) return;
 
