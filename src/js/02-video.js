45174dea2f33785116781fcffe2776194c25debb
Oleh Protasevych <protasevicholeg@gmail.com>
2023-03-11 04:23:59 +0200

    Trying to fix function for the 3rd time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,14 +5,15 @@ import throttle from 'lodash.throttle';
 const player = new Player('vimeo-player');
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(savePlaybackPosition, 1000));
+
 function savePlaybackPosition(data) {
   localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify(data.seconds)
   );
 }
-
-if (!currentTime) return;
-
-player.setCurrentTime(currentTime);
-player.on('timeupdate', throttle(savePlaybackPosition, 1000));
--- src/js/utils/savePlaybackPosition.js
+++ src/js/utils/savePlaybackPosition.js
@@ -1,6 +1,6 @@
-export default function savePlaybackPosition(data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
-}
+// export default function savePlaybackPosition(data) {
+//   localStorage.setItem(
+//     'videoplayer-current-time',
+//     JSON.stringify(data.seconds)
+//   );
+// }
